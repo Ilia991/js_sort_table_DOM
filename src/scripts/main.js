d9e1742b915e31d0b8e895b2d2f64ead1b278936
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-12 20:03:43 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+const thead = table.querySelector('thead');
+const ths = thead.querySelectorAll('th')
+
+function sortTable(columnIndex) {
+  const tbody = table.querySelector('tbody');
+  const [...rows] = tbody.querySelectorAll('tr');
+  
+  rows.forEach(row => tbody.removeChild(row));
+
+  rows.sort((a, b) => {
+    const aCellValue = a.querySelectorAll('td')[columnIndex].textContent;
+    const bCellValue = b.querySelectorAll('td')[columnIndex].textContent;
+
+    return aCellValue.localeCompare(bCellValue, undefined, { numeric: true });
+  });
+  
+  rows.forEach(row => tbody.appendChild(row));
+}
+
+ths.forEach((th, i) => {
+  th.addEventListener('click', () => sortTable(i));
+});
